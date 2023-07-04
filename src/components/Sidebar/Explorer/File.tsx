c38fd76b28861943499cad548f989ad36851c543
Himanshu Pal <mailtohimanshupal@gmail.com>
2023-07-05 02:27:41 +0530

    Added icons for file

--- src/components/Sidebar/Explorer/File.tsx
+++ src/components/Sidebar/Explorer/File.tsx
@@ -1,5 +1,6 @@
 import { useEditorContext } from '@/context/EditorContext'
 import { getLanguageForFileName, join } from '@/utils'
+import FileIcon from '@/components/FileIcon'
 import { isFile } from '@/utils/detectType'
 import { useSidebarStore } from '@/store'
 import { Fragment, memo } from 'react'
@@ -7,7 +8,6 @@ import { Fragment, memo } from 'react'
 import { ReactComponent as ChevronRight } from '@/assets/icons/chevron-right.svg'
 import { ReactComponent as ChevronDown } from '@/assets/icons/chevron-down.svg'
 import { ReactComponent as DeleteIcon } from '@/assets/icons/delete.svg'
-import { ReactComponent as FileIcon } from '@/assets/icons/file.svg'
 
 import NewFileInput from './NewFileInput'
 import type { ITreeProps } from './Tree'
@@ -21,6 +21,7 @@ export interface IFileProps extends Omit<ITreeProps, 'rootId'> {
 const File = memo(({ content: f, renderInput, level, deleteFileOrFolder, forceRenderInput, isRoot }: IFileProps) => {
 	const { selectedItem, setSelectedItem, newFile } = useSidebarStore()
 	const { createModel, expandFolders } = useEditorContext()
+	const fileLanguage = getLanguageForFileName(f.name)
 
 	return (
 		<Fragment>
@@ -30,14 +31,12 @@ const File = memo(({ content: f, renderInput, level, deleteFileOrFolder, forceRe
 					style={level ? { paddingLeft: level * 12 } : undefined}
 					className={join('pointer', style.file, selectedItem?.id === f.id && style.fileSelected)}
 					onClick={(e) => (
-						e.stopPropagation(),
-						setSelectedItem(f),
-						isFile(f) ? createModel(getLanguageForFileName(f.name), f.name, f.id) : expandFolders([f.id], !f.isExpanded)
+						e.stopPropagation(), setSelectedItem(f), isFile(f) ? createModel(fileLanguage, f.name, f.id) : expandFolders([f.id], !f.isExpanded)
 					)}
 				>
 					<span className="row" style={{ gap: isFile(f) ? 4 : 2, alignItems: 'center' }}>
 						{isFile(f) ? (
-							<FileIcon width={12} height={12} />
+							<FileIcon fileName={f.name} />
 						) : f.isExpanded ? (
 							<ChevronDown width={14} height={14} />
 						) : (
